f361e3bde3fecd9d727edcb689fcb4e82b2bfcf5
khuysor <huysorkheang22@gmail.com>
2024-06-26 00:14:35 +0700

    create user login and register page with api

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,7 +1,8 @@
 import { Button, Form, Input, Typography, message } from "antd";
 import { createSession, login } from "../../util/auth";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useState } from "react";
+import { routes } from "../../routes/routes";
 const Login = () => {
   const nav = useNavigate();
   message.useMessage();
@@ -11,7 +12,8 @@ const Login = () => {
     setLoading(true);
     await login(values.username, values.password)
       .then((res: any) => {
-        createSession(res.data);
+        // createSession(res.data);
+        console.log(res.data);
         message.success("Login Success");
         nav("/");
       })
@@ -34,48 +36,43 @@ const Login = () => {
         layout={"vertical"}
         requiredMark={false}
       >
-      
-          <Form.Item
-            name="username"
-            label={
-              <p className="block text-sm font-medium text-gray-900">User Name</p>
-            }
-            rules={[
-              {
-                required: true,
-                message: "Please enter your email",
-              },
-              {
-                type: "string",
-              },
-            ]}
-          >
-            <Input className="bg-gray-50 text-gray-900 sm:text-sm py-1.5" />
-          </Form.Item>
+        <Form.Item
+          name="username"
+          label={
+            <p className="block text-sm font-medium text-gray-900">User Name</p>
+          }
+          rules={[
+            {
+              required: true,
+              message: "Please enter your email",
+            },
+            {
+              type: "string",
+            },
+          ]}
+        >
+          <Input className="bg-gray-50 text-gray-900 sm:text-sm py-1.5" />
+        </Form.Item>
 
+        <Form.Item
+          name="password"
+          label={
+            <p className="block text-sm font-medium text-gray-900">Password</p>
+          }
+          rules={[
+            {
+              required: true,
+              message: "Please enter your password",
+            },
+          ]}
+        >
+          <Input.Password
+            visibilityToggle={false}
+            className="bg-gray-50 text-gray-900 sm:text-sm py-1.5"
+          />
+        </Form.Item>
 
-          <Form.Item
-            name="password"
-            label={
-              <p className="block text-sm font-medium text-gray-900">
-                Password
-              </p>
-            }
-            rules={[
-              {
-                required: true,
-                message: "Please enter your password",
-              },
-            ]}
-          >
-            <Input.Password
-              visibilityToggle={false}
-              className="bg-gray-50 text-gray-900 sm:text-sm py-1.5"
-            />
-          </Form.Item>
-     
-
-        <div className="text-center">
+        <div className="text-center ">
           <Button
             className="mt-4 bg-primary"
             block
@@ -86,6 +83,14 @@ const Login = () => {
           >
             Login
           </Button>
+          <div className="mt-4">
+            <Link
+              className=" underline text-blue-700"
+              to={`${routes.userregister}`}
+            >
+              Sign Up
+            </Link>
+          </div>
         </div>
       </Form>
     </>
--- src/components/auth/register.tsx
+++ src/components/auth/register.tsx
@@ -1,19 +1,57 @@
-import { Input, Button, Form } from "antd";
+import { Input, Button, Form, message } from "antd";
+import { useState } from "react";
+import { createSession, registerUrl, userRegister } from "../../util/auth";
+import axios from "axios";
+import { Link, useNavigate } from "react-router-dom";
+import { routes } from "../../routes/routes";
 
 const Register = () => {
+  const [password, setPassword] = useState<string>("");
+  const [loading, setLoading] = useState(false);
+  const validatePasswordConfirm = async (_, value: string) => {
+    if (value && value !== password) {
+      throw new Error("The two passwords that you entered do not match!");
+    }
+  };
+  const nav = useNavigate();
+  const register = async (value: userRegister) => {
+    const newUser = {
+      firstname: value.firstname,
+      lastname: value.lastname,
+      username: value.username,
+      password: value.password,
+      role: "ADMIN",
+    };
+
+    setLoading(true);
+    await axios
+      .post(registerUrl, newUser)
+      .then((res) => {
+        createSession(res.data);
+        message.success("Login Success");
+        nav("/");
+      })
+      .catch((e) => {
+        setLoading(false);
+        message.error("Invalid User name or Password");
+        setLoading(false);
+        throw new Error(e);
+      });
+  };
   return (
     <>
-      <h1 className=" text-2xl font-bold leading-tight tracking-tight md:text-2xl text-left mb-6 text-blue-500 text-opacity-90">
+      <h1 className=" text-2xl font-bold leading-tight tracking-tight md:text-2xl text-left mb-5 text-blue-500 text-opacity-90">
         Admin Register
       </h1>
       <Form
         className="space-y-4 md:space-y-6"
         name="login"
         layout={"vertical"}
+        onFinish={register}
         requiredMark={false}
       >
         <Form.Item
-          name="username"
+          name="firstname"
           label={
             <p className="block text-sm font-medium text-gray-900">
               First Name
@@ -22,7 +60,7 @@ const Register = () => {
           rules={[
             {
               required: true,
-              message: "Please enter your email",
+              message: "Please enter your first name",
             },
             {
               type: "string",
@@ -32,14 +70,14 @@ const Register = () => {
           <Input className="bg-gray-50 text-gray-900 sm:text-sm py-1.5" />
         </Form.Item>
         <Form.Item
-          name="username"
+          name="lastname"
           label={
             <p className="block text-sm font-medium text-gray-900">Last Name</p>
           }
           rules={[
             {
               required: true,
-              message: "Please enter your email",
+              message: "Please enter your ",
             },
             {
               type: "string",
@@ -57,7 +95,7 @@ const Register = () => {
           rules={[
             {
               required: true,
-              message: "Please enter your email",
+              message: "Please enter your username",
             },
             {
               type: "string",
@@ -80,13 +118,14 @@ const Register = () => {
           ]}
         >
           <Input.Password
-            visibilityToggle={false}
+            visibilityToggle={true}
+            onChange={(e) => setPassword(e.target.value)}
             className="bg-gray-50 text-gray-900 sm:text-sm py-1.5"
           />
         </Form.Item>
 
         <Form.Item
-          name="password"
+          name="passwordcf"
           label={
             <p className="block text-sm font-medium text-gray-900">Password</p>
           }
@@ -95,10 +134,13 @@ const Register = () => {
               required: true,
               message: "Please enter your password",
             },
+            {
+              validator: validatePasswordConfirm,
+            },
           ]}
         >
           <Input.Password
-            visibilityToggle={false}
+            visibilityToggle={true}
             className="bg-gray-50 text-gray-900 sm:text-sm py-1.5"
           />
         </Form.Item>
@@ -107,12 +149,18 @@ const Register = () => {
           <Button
             className="mt-4 bg-primary"
             block
+            loading={loading}
             type="primary"
             size="large"
             htmlType={"submit"}
           >
-            Login
+            Register
           </Button>
+          <div className="mt-4">
+            <Link className=" underline text-blue-700 " to={`${routes.login}`}>
+              Already Have Account
+            </Link>
+          </div>
         </div>
       </Form>
     </>
