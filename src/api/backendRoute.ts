6b86cb268f2d5be7dce14a2707039c3582de0c6e
Huysor kheang <lokmjassor@gmail.com>
2024-10-31 21:36:47 +0700

    api backend

---
+++ src/api/backendRoute.ts
@@ -0,0 +1,17 @@
+const root = "http://localhost:8080";
+// authentication
+export const apiLogin = root + "/auth/authentication";
+export const apiRegister = root + "/auth/register";
+
+// category
+export const apiCategory = root + "/api/category";
+export const apiAllCategory = root + "/api/category/all";
+export const apiCategoryCount = root + "/api/category/count";
+
+// course
+export const apiCourse = root + "/api/course";
+export const apiCourseCount = root + "/api/course/count";
+
+// student
+export const apiStudent = root + "/api/student";
+export const apiStudentCount = root + "/api/student/count";
